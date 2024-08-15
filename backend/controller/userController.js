590aaf920f613a868f0e25a2f12b288aa2f433fb
neelanshagr <neelanshagrawal16@gmail.com>
2024-08-15 23:28:39 +0530

    admin registration added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -40,3 +40,67 @@ export const patientRegister = catchAsyncError(async (req, res, next) => {
   });
   generateToken(user, "User Registered!", 200, res);
 });
+
+export const login = catchAsyncError(async (req, res, next) => {
+  const { email, password, confirmPassword, role } = req.body;
+  if (!email || !password || !confirmPassword || !role) {
+    return next(new ErrorHandler("Please Fill Full Form!", 400));
+  }
+  if (password !== confirmPassword) {
+    return next(
+      new ErrorHandler("Password & Confirm Password Do Not Match!", 400)
+    );
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email Or Password!", 400));
+  }
+
+  const isPasswordMatch = await user.comparePassword(password);
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Invalid Email Or Password!", 400));
+  }
+  if (role !== user.role) {
+    return next(new ErrorHandler(`User Not Found With This Role!`, 400));
+  }
+  generateToken(user, "Login Successfully!", 201, res);
+});
+
+export const addNewAdmin = catchAsyncError(async (req, res, next) => {
+  const { firstName, lastName, email, phone, nic, dob, gender, password } =
+    req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !nic ||
+    !dob ||
+    !gender ||
+    !password
+  ) {
+    return next(new ErrorHandler("Please Fill Full Form!", 400));
+  }
+
+  const isRegistered = await User.findOne({ email });
+  if (isRegistered) {
+    return next(new ErrorHandler(`${isRegistered.role} With This Email Already Exists!`, 400));
+  }
+
+  const admin = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    nic,
+    dob,
+    gender,
+    password,
+    role: "Admin",
+  });
+  res.status(200).json({
+    success: true,
+    message: "New Admin Registered",
+    admin,
+  });
+});
--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,8 +1,10 @@
 import  express from "express";
-import { patientRegister } from "../controller/userController.js";
+import { patientRegister, login, addNewAdmin, } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.post("/patient/register",patientRegister);
+router.post("/login", login);
+router.post("/admin/addnew", addNewAdmin)
 
 export default router;   
