db0d09db59432d232c7326c7779f8947bd51096b
Kesar25 <117337016+Kesar25@users.noreply.github.com>
2024-08-19 23:51:56 +0530

    frontend setup, creating context

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Appointment from './pages/Appointment'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import About from './pages/About'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/appointment" element={<Appointment/>}/>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/register" element={<Register/>}></Route>
+        </Routes>
+        <ToastContainer position='top-center'></ToastContainer>
+      </Router>
+    </>
+  )
+}
+
+export default App
