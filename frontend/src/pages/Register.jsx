e06f9cea49965f58c10761232d2fbf9f71db0120
Kesar25 <117337016+Kesar25@users.noreply.github.com>
2024-08-21 23:33:01 +0530

    Creating Register page with styling

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,140 @@
-import React from 'react'
+import axios from "axios";
+import { useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { Context } from "../main";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import './Register.css';
 
 const Register = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
 
-export default Register
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [UHID, setUHID] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigateTo = useNavigate();
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+    try {
+      await axios
+        .post(
+          "http://localhost:4002/api/v1/user/patient/register",
+          { firstName, lastName, email, phone, UHID, dob, gender, password, role: "Patient" },
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((res) => {
+          toast.success(res.data.message);
+          setIsAuthenticated(true);
+          navigateTo("/");
+          setFirstName("");
+          setLastName("");
+          setEmail("");
+          setPhone("");
+          setUHID("");
+          setDob("");
+          setGender("");
+          setPassword("");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <div className="register-container">
+      <div className="register-form">
+        <h2 className="register-title">Sign Up</h2>
+        <p className="register-subtitle">Please Sign Up To Continue</p>
+        
+        <form onSubmit={handleRegistration}>
+          <div className="register-form-fields">
+            <input
+              type="text"
+              className="register-input"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              type="text"
+              className="register-input"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <input
+              type="text"
+              className="register-input"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="number"
+              className="register-input"
+              placeholder="Mobile Number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <input
+              type="number"
+              className="register-input"
+              placeholder="UHID"
+              value={UHID}
+              onChange={(e) => setUHID(e.target.value)}
+            />
+            <input
+              type="date"
+              className="register-input"
+              placeholder="Date of Birth"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+            />
+            <select
+              className="register-input"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+            >
+              <option value="">Select Gender</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+            <input
+              type="password"
+              className="register-input"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="register-footer">
+            <p className="register-footer-text">Already Registered?</p>
+            <Link to={"/login"} className="register-login-link">
+              Login Now
+            </Link>
+          </div>
+          <div className="register-footer">
+            <button type="submit" className="register-button">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
